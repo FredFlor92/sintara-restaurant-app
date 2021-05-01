b00693a8392d2802ab936e02e7425d3c661c71d1
freddyflorial@gmail.com <freddyflorial@gmail.com>
2021-05-01 12:21:46 -0400

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,10 @@ class ApplicationController < Sinatra::Base
 
 
   configure do
+    set :public_folder, 'public'
+   set :views, 'app/views'
    enable :sessions
    set :session_secret, "RestuarantReviewApp"
-
-   set :public_folder, 'public'
-   set :views, 'app/views'
   end
 
   get "/" do

0f000e8358893e37cb5b0bcb9bdfe9ad7b4920ed
freddyflorial@gmail.com <freddyflorial@gmail.com>
2021-05-01 23:08:00 -0400

    create journal entries edit route and link

--- app/controllers/restaurant_entries_controller.rb
+++ app/controllers/restaurant_entries_controller.rb
@@ -1,5 +1,30 @@
 class RestaurantEntriesController < ApplicationController
-     get '/restaurant_entries/new' do
-         erb :'/restaurant_entries/new'
-     end 
+    get '/restaurant_entries/new' do
+        erb :'/restaurant_entries/new'
+    end 
+
+    post /'restaurant_entries' do
+        if !logged_in?
+            redirect '/'
+        end
+        
+        #check with hov video 4 52mins
+        if  params[:content] != ""
+            @restaurant_entry = RestaurantEntry.create(content: params[:content], user_id: current_user.id)
+            redirect "/restaurant_entries/#{@restaurant_entry.id}"
+        else
+            redirect '/restaurant_entries/new'
+        end
+    end
+
+    get '/restaurant_entries/:id' do
+        @restaurant_entry = RestaurantEntry.find(params[:id])
+        erb :'/restaurant_entries/show'
+    end
+
+    get '/restaurant_entries/:id/edit' do
+       erb :/'restaurant_entries/edit'
+    end
+
+
 end 

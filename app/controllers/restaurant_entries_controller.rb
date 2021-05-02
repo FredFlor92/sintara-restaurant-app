c47de71dbe90453f826996eadc0e520227d7a32d
freddyflorial@gmail.com <freddyflorial@gmail.com>
2021-05-02 08:05:17 -0400

    add index action to entrys

--- app/controllers/restaurant_entries_controller.rb
+++ app/controllers/restaurant_entries_controller.rb
@@ -1,4 +1,10 @@
 class RestaurantEntriesController < ApplicationController
+    
+    get '/restaurant_entries' do
+        @restaurant_entries = @RestaurantEntry.all
+        erb :'restaurant_entries/index'
+    end
+    
     get '/restaurant_entries/new' do
         erb :'/restaurant_entries/new'
     end 
@@ -18,13 +24,34 @@ class RestaurantEntriesController < ApplicationController
     end
 
     get '/restaurant_entries/:id' do
-        @restaurant_entry = RestaurantEntry.find(params[:id])
+        set_restaurant_entry
         erb :'/restaurant_entries/show'
     end
 
     get '/restaurant_entries/:id/edit' do
+        set_restaurant_entry
+        if logged_in?
+        if authorized_to_edit?(@restaurant_entry)
        erb :/'restaurant_entries/edit'
+        else
+            redirect "users/#{current_user.id}"
+        end
+        else
+            redirect'/'
+        end
+    end  
+    patch '/restaurant_entries/:id' do
+        set_restaurant_entry
+        if  logged_in?
+           if @restaurant_entry.update(content: params[:content])
+            redirect "/restaurant_entries/#{@restaurant_entry.id}"
+        else
+            redirect "users/#{current_user.id}"
+        end
+    end 
+    
+    private
+    def set_restaurant_entry
+        @restaurant_entry = RestaurantEntry.find(params[:id])
     end
-
-
 end 

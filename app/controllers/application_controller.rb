a76a724a9ac10bef99f32a293beabe49cbc49cea
freddyflorial@gmail.com <freddyflorial@gmail.com>
2021-05-02 13:02:07 -0400

    lastest version of website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,16 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-   set :views, 'app/views'
-   enable :sessions
-   set :session_secret, "RestuarantReviewApp"
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "RestuarantReviewApp"
   end
 
   get "/" do
     if logged_in?
       redirect "/users/#{current_user.id}"
     else
-    erb :welcome
+      erb :welcome
     end
 
   end
--- app/controllers/restaurant_entries_controller.rb
+++ app/controllers/restaurant_entries_controller.rb
@@ -2,20 +2,20 @@ class RestaurantEntriesController < ApplicationController
     
     get '/restaurant_entries' do
         @restaurant_entries = @RestaurantEntry.all
-        erb :'restaurant_entries/index'
+        erb :'/restaurant_entries/index'
     end
     
     get '/restaurant_entries/new' do
         erb :'/restaurant_entries/new'
     end 
 
-    post /'restaurant_entries' do
-        if !logged_in?
+    post '/restaurant_entries/new' do
+        if 
+            !logged_in?
             redirect '/'
         end
-        
-        #check with hov video 4 52mins
-        if  params[:content] != ""
+        if  
+            params[:content] != ""
             @restaurant_entry = RestaurantEntry.create(content: params[:content], user_id: current_user.id)
             redirect "/restaurant_entries/#{@restaurant_entry.id}"
         else
@@ -28,14 +28,15 @@ class RestaurantEntriesController < ApplicationController
         erb :'/restaurant_entries/show'
     end
 
-    get '/restaurant_entries/:id/edit' do
+    get '/restaurant_entries/restaurant_entries/:id/edit' do
         set_restaurant_entry
         if logged_in?
-        if authorized_to_edit?(@restaurant_entry)
-       erb :/'restaurant_entries/edit'
-        else
-            redirect "users/#{current_user.id}"
-        end
+            if authorized_to_edit?(@restaurant_entry)
+                erb :'/restaurant_entries/edit'
+            
+            else
+                redirect "users/#{current_user.id}"
+            end
         else
             redirect'/'
         end
@@ -43,15 +44,26 @@ class RestaurantEntriesController < ApplicationController
     patch '/restaurant_entries/:id' do
         set_restaurant_entry
         if  logged_in?
-           if @restaurant_entry.update(content: params[:content])
-            redirect "/restaurant_entries/#{@restaurant_entry.id}"
+            if @restaurant_entry.update(content: params[:content])
+                redirect "/restaurant_entries/#{@restaurant_entry.id}"
+            end
         else
             redirect "users/#{current_user.id}"
         end
     end 
     
+    delete '/restaurant_entries/:id' do
+        set_restaurant_entry
+        if authorized_to_edit?(@restaurant_entry)
+         @restaurant_entry.destroy
+         redirect '/restaurant_entries' 
+        else
+            redirect "users/#{current_user.id}"
+        end   
+    end
+    
     private
     def set_restaurant_entry
-        @restaurant_entry = RestaurantEntry.find(params[:id])
+        @restaurant_entry = RestaurantEntry.find_by(params[:id])
     end
 end 
